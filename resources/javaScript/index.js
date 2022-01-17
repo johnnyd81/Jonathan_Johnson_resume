da6a063046df7e447e7700b01192eff3e2763759
Jonathan Johnson <95863021+johnnyd81@users.noreply.github.com>
2022-01-17 22:35:11 +0200

    added new file index.html and resources folder

---
+++ resources/javaScript/index.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('.header,.about,.education,.work,.skills,.project').height($(window).height());
+}) // this code makes each section take the height of the window
+
+$(".navbar a").click(function(){
+  $("body,html").animate({
+   scrollTop:$("#" + $(this).data('value')).offset().top
+  },1000)
+  
+ }) // this code creates the scroll effect when the link on the navigation bar is clicked
